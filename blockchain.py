e99ab8fdb878a88ac28338ac6fe3c1653bba5766
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-07-19 07:16:59 -0400

    Initial Commit

---
+++ blockchain.py
@@ -0,0 +1,62 @@
+import hashlib
+import json
+from time import time
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
+
+    '''
+    Creates a new Block in the Blockchain
+    :param proof: <int> The proof given by the Proof of Work algorithm
+    :param previous_hash: (Optional) <str> Hash of the previous Block
+    :return: <dict> New Block
+    '''
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    '''
+    Creates a new transaction to go into the next mined Block
+    :param sender: <str> Address of the Sender
+    :param recipient: <str> Address of the Recipient
+    :param amount: <int> Amount
+    :return: <int> The index of the Block that will hold this transaction
+    '''
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    '''
+    Creates a SHA-265 hash of a Block
+    :param block: <dict> Block
+    :return: <str>
+    '''
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    # Returns the last Block in the chain
+    @property
+    def last_block(self):
+        return self.chain[-1]
